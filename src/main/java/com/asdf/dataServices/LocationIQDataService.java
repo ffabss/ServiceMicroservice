94f3e1eb751bcb1f83200fcc84323072459d1392
ffabss <37747351+ffabss@users.noreply.github.com>
2020-04-24 18:27:41 +0200

    Update LocationIQDataService.java

--- src/main/java/com/asdf/dataServices/LocationIQDataService.java
+++ src/main/java/com/asdf/dataServices/LocationIQDataService.java
@@ -15,8 +15,8 @@ public class LocationIQDataService {
     private static final String LOCATION_IQ_URL = "https://eu1.locationiq.com/v1/search.php?key={apiKey}&q={searchString}&format=json";
     private static final String LOCATION_IQ_REVERSE_URL = "https://eu1.locationiq.com/v1/reverse.php?key={apiKey}&lat={lat}&lon={lon}&format=json";
 
-    private final int MAX_TRIES = 50;
-    private int currentKeyID = 0;
+    private final int MAX_TRIES = 100;
+    private static int currentKeyID = 0;
     private static final String[] API_KEYs = {"34f5a525bf298a", "b06efbe4532f08",
             "201e218b2b48a4", "1d6c03484794a5", "6ddac64cdf0664", "4f7a4411e0fcbb",
             "a45f0df77338d9", "21fe6b3316a50b", "e2107f8b1b287f", "d881a7874b8938"};
@@ -25,7 +25,7 @@ public class LocationIQDataService {
         return API_KEYs[currentKeyID];
     }
 
-    private void changeKey() {
+    private static void changeKey() {
         currentKeyID++;
         if (currentKeyID == API_KEYs.length) {
             currentKeyID = 0;
@@ -37,6 +37,21 @@ public class LocationIQDataService {
     }
 
     public LongitudeLatitude getLongitudeLatitudeByAddress(String address, int triesLeft) {
+        changeKey();
+        if (triesLeft < MAX_TRIES) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+            }
+        } else {
+            try {
+                Thread.sleep((int) ((500 / API_KEYs.length) * 1.5));
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+            }
+        }
+
         RestTemplate restTemplate = new RestTemplate();
         Map<String, String> vars = new HashMap<>();
         vars.put("apiKey", getCurrentKey());
@@ -50,8 +65,7 @@ public class LocationIQDataService {
             result.setLatitude(response[0].getLat());
             return result;
         } catch (RestClientResponseException e) {
-            if (e.getRawStatusCode() == 429 && triesLeft > 0) {
-                changeKey();
+            if ((e.getRawStatusCode() == 429 || e.getRawStatusCode() == 404) && triesLeft > 0) {
                 return getLongitudeLatitudeByAddress(address, triesLeft - 1);
             } else {
                 throw new InternalServerException(e);
